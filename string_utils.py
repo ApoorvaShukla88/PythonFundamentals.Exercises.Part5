07569db4987d161948c5d2e78b01310310592c9d
Apoorva Mishra <apoorvashukla88@gmail.com>
2020-03-07 09:56:46 -0500

    complete functions

--- string_utils.py
+++ string_utils.py
@@ -3,40 +3,46 @@ def str_len(str_in: str) -> str:
     """
     Given a string parameter, this function should return the length of the parameter.
     """
-    pass  # remove pass statement and implement me
+
+    return len(str_in)
 
 
 def first_char(str_in: str) -> str:
     """
     Given a string parameter, this function should return the first letter of the parameter.
     """
-    pass  # remove pass statement and implement me
+    return str_in[:1]
 
 
 def last_char(str_in: str) -> str:
     """
     Given a string parameter, this function should return the last letter of the parameter..
     """
-    pass  # remove pass statement and implement me
+    return str_in[-1:] # remove pass statement and implement me
 
 
 def input_has_substring(str_in: str, sub_str_in: str) -> bool:
     """
     This function determines if the substring exists within the string. Returns True or False.
     """
-    pass  # remove pass statement and implement me
+    if str_in.find(sub_str_in) == -1:
+        return False
+    else:
+        return True
+
+    # remove pass statement and implement me
 
 
 def substring(str_in: str, start: int, stop: int) -> str:
     """
     Returns the substring of a string.
-
     Keyword arguments:
     str_in -- the string in which to generate a substring from
     start -- starting position of the input parameter to start the substring (inclusive)
     stop -- stopping position of the input parameter to stop the substring (exclusive)
     """
-    pass  # remove pass statement and implement me
+    return str_in[start:stop]
+    # remove pass statement and implement me
 
 
 def opposite_case(str_in: str) -> str:
@@ -45,4 +51,9 @@ def opposite_case(str_in: str) -> str:
     Example: 
     When input = "Python" the function returns "pYTHON"
     """
-    pass  # remove pass statement and implement me
+    return str_in.swapcase() # remove pass statement and implement me
+
+
+#print(str_len('test'))
+
+print(input_has_substring("I am Beginner", "am"))
